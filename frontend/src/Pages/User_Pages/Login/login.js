7f64b4a9026d19bb81789b6e1c682c7010dc65a1
Xperior28 <rohithraichu@gmail.com>
2023-12-16 18:48:59 +0530

    made basic non function UI of the user login

--- frontend/src/Pages/User_Pages/Login/login.js
+++ frontend/src/Pages/User_Pages/Login/login.js
@@ -1,10 +1,29 @@
 import React from "react";
+import './login.css';
+
+
+function Details(props) {
+    return(
+        <div>
+            <label>{props.label}</label><br></br>
+            <input type="text" className="textarea"></input>
+        </div>
+    );
+}
 
 function Login(){
     return (
-        <div>
-            <h1>Hello World</h1>
+        <>
+        <div className = "container">
+            <div className="box">
+                <h1>User Login</h1>
+                <Details label="email" />
+                <Details label="password" />
+                <button className="submit" >Submit</button>
+                <div className="signup">Are you new? <a href="https://www.youtube.com/" target="_blank">Register here</a></div>
+            </div>
         </div>
+        </>
     )
 }
 
