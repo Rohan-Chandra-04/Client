c0e34e4b29ef5023a35f8a6361b0ffe1b62dac1a
Vaibhav <vaibhavagarwal693@gmail.com>
2024-03-08 17:52:40 +0530

    current work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,38 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './Pages/Home/homepage';
-import UserLogin from './Pages/User_Pages/Login/login';
-import ShopKeeperLogin from './Pages/ShopKeeper_Pages/Login/login';
-
-import AdminLogin from './Pages/Admin_Pages/Login/login';
-
-import UserSignUp from './Pages/User_Pages/SignUp/signup';
-import ShopKeeperSignUp from './Pages/ShopKeeper_Pages/SignUp/signup';
-// import AdminLogin from './Pages/Admin_Pages/Login/login';
-import UserHome from './Pages/User_Pages/Home/userhome';
-import ShopKeeperHome from './Pages/ShopKeeper_Pages/Home/shopkeeperhomepage';
-
-import './App.css';
 import LandingPage from './Pages/Landing_Page/landing_page';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Contact from './Pages/Contact_page/contact';
 import Help from './Pages/help_page/help';
+import UserLogin from './Pages/User_Pages/Login/login';
+import ShopKeeperLogin from './Pages/ShopKeeper_Pages/Login/login';
+import AdminLogin from './Pages/Admin_Pages/Login/login';
 import Home from './Pages/Home_Page/home';
-import Profile from './Pages/Profile_Page/profile';
-import   ShopLanding from './Pages/ShopKeeper_Pages/Login/shop_landing page/shop_landing';
+// import Profile from './Pages/Profile_Page/profile';
+import ShopLanding from './Pages/ShopKeeper_Pages/Login/shop_landing page/shop_landing';
+import UserHome from './Pages/User_Pages/Home/userhome';
+import ShopKeeperHome from './Pages/ShopKeeper_Pages/Home/shopkeeperhomepage';
 
 function App() {
   return (
     <div className="App">
-
-    <Router>
-      <Routes>
-        <Route path="/" element= { <LandingPage/>}/>
-        <Route path="/contact" element={<Contact/>}></Route>
-        <Route path="/help" element={<Help/>}/>
-        <Route path="/userlogin" element={<UserLogin/>}/>
-        <Route path="/shopkeeperlogin" element={<ShopKeeperLogin/>}/>
-        <Route path="/adminlogin" element={<AdminLogin/>}/>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/shoplanding" element={<ShopLanding/>}></Route>
-
-        
-      </Routes>
-    </Router>
-      
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/help" element={<Help />} />
+          <Route path="/userlogin" element={<UserLogin />} />
+          <Route path="/shopkeeperlogin" element={<ShopKeeperLogin />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/profile" element={<Profile />} /> */}
+          <Route path="/shoplanding" element={<ShopLanding />} />
+          <Route path='/userHome' element={<UserHome />} />
+          <Route path='/shopKeeperHome' element={<ShopKeeperHome />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
-
