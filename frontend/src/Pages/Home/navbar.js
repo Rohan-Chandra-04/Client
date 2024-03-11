d0cc4f9b5a74431840972a6ff6026c1d5e4737a7
Xperior28 <rohithraichu@gmail.com>
2024-03-11 21:31:41 +0530

    login button added to landing page

--- frontend/src/Pages/Home/navbar.js
+++ frontend/src/Pages/Home/navbar.js
@@ -1,16 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import './navbar.css'
+import { useNavigate } from 'react-router-dom';
+
+function LoginSubWrap(props) {
+
+    const navigate = useNavigate();
+
+    if(props.isVisible) {
+        return (
+            <>
+                <div className="login-subwrap" id="submenu">
+                    <h4 className="login-subwrap-option" onClick={() => navigate("/UserLogin")}>User Login</h4>
+                    <h4 className="login-subwrap-option"  onClick={() => navigate("/ShopKeeperLogin")}>Shopkeeper Login</h4>
+                    <h4 className="login-subwrap-option"  onClick={() => navigate("/AdminLogin")}>Admin Login</h4>
+                </div>
+            </>
+        );
+    }
+}
 
 export default function Navbar() {
+    
+
+    const [subWrap, setSubWrap] = React.useState(false);
+
     return(
         <>
         <div className="navbar">
             <div className="nav-logo"></div>
-            <Link to="/UserLogin" className="link"> UserLogin </Link>
-            <Link to="/ShopKeeperLogin" className="link"> ShopKeeperLogin </Link>
-            <Link to="/AdminLogin" className="link"> AdminLogin </Link>
-            
+            <button className="login-btn" onClick={() => setSubWrap(!subWrap)}>Login</button>
+            <LoginSubWrap isVisible={subWrap}/>
         </div>
         </>
     );
